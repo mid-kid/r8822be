79949c0dde5bab08778c972e799f77e4e62c3526
mid-kid <esteve.varela@gmail.com>
2020-03-31 15:39:52 +0200

    Revert "mac80211: simplify TX aggregation start"
    This reverts commit 56fb4ec237ab98813035f848ee04593cc692b5bf.

--- base.c
+++ base.c
@@ -1722,7 +1722,8 @@ int rtl_tx_agg_start(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 	*ssn = tid_data->seq_number;
 	tid_data->agg.agg_state = RTL_AGG_START;
 
-	return IEEE80211_AMPDU_TX_START_IMMEDIATE;
+	ieee80211_start_tx_ba_cb_irqsafe(vif, sta->addr, tid);
+	return 0;
 }
 
 int rtl_tx_agg_stop(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
